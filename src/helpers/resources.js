3afe93672f55959f1cc345426f99798419f384fd
Mark Moloney <markmo@me.com>
2016-12-05 00:18:07 +1100

    rebuild dist

--- src/helpers/resources.js
+++ src/helpers/resources.js
@@ -2,9 +2,9 @@ import Vue from 'vue';
 import VueResource from 'vue-resource';
 import { router } from 'src/main';
 
-const API_BASE = 'https://tombot1.ngrok.io';
+// const API_BASE = 'https://tombot1.ngrok.io';
 // const API_BASE = 'http://localhost:8080';
-// const API_BASE = 'http://localhost:5555';
+const API_BASE = 'http://localhost:5555';
 
 Vue.use(VueResource);
 
